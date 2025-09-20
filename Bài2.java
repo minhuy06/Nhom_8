434cd6eb658cfb7bfa68c7a9c5afcfd535a54d2b
taminhdatta-ui <taminhdatta@gmail.com>
2025-09-21 03:57:12 +0700

    Add files via upload

--- Bài2.java
+++ Bài2.java
@@ -1,17 +1,24 @@
-package baitap1;
+package baitap2;
+
+import java.util.Scanner;
 
 public class Bài2 {
+	static int a, b, c, min;
+
 	public static void main(String[] args) {
-		char[] myName = { 'Đ', 'Ạ', 'T' };
-		int myAge = 19;
-		double myHeight = 1.75;
-		boolean myAnswer = true;
-		System.out.println("Tên: " + new String(myName));
-		System.out.println("Tuổi: " + myAge);
-		System.out.println("Chiều cao: " + myHeight);
-		if (myAnswer == true)
-			System.out.println("Tôi thích lập trình ");
-		else
-			System.out.println("Tôi không thích lập trình ");
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Nhap so a: ");
+		a = scanner.nextInt();
+		System.out.print("Nhap so b: ");
+		b = scanner.nextInt();
+		System.out.print("Nhap so c: ");
+		c = scanner.nextInt();
+		min = a;
+		if (b < min)
+			min = b;
+		if (c < min)
+			min = c;
+		System.out.println("So nho nhat la: " + min);
+		scanner.close();
 	}
 }
